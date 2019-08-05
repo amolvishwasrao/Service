1f2154e977886861377c6f02d8c7e02a7cdd6c54
amolvishwasrao <vishwasrao.amol@gmail.com>
2019-08-05 20:12:12 +0530

    Add files via upload

---
+++ app/batch-details/batch-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DemoService } from '../demo.service';
+
+@Component({
+  selector: 'app-batch-details',
+  templateUrl: './batch-details.component.html',
+  styleUrls: ['./batch-details.component.css']
+})
+export class BatchDetailsComponent implements OnInit 
+{
+  public batches=[];
+
+  constructor(public obj:DemoService)
+  {
+     this.batches=this.obj.GetBatchDetails();
+
+  }
+  // Same array is used in both components
+  // public batches = 
+  // [
+  //   {"Name":"PPA", "Fees":9000, "Duration":"4 Months"},
+  //   {"Name":"LB", "Fees":7500, "Duration":"4 Months"},
+  //   {"Name":"WEB", "Fees":2500, "Duration":"3 Months"},
+  //   {"Name":"Project", "Fees":4000, "Duration":"3 Months"},
+  // ];
+
+  ngOnInit() {
+  }
+
+}
